6e6b524ed8aebe60123d25f8ef1bb904cf79783b
Himani <himani9.engg@gmail.com>
2022-01-22 16:28:44 +0530

    removing a callback

--- src/components/App.js
+++ src/components/App.js
@@ -20,9 +20,9 @@ const App = () => {
     setVideos(response.data.items);
     setSelectedVideo(response.data.items[0]);
   };
-  const onVideoSelect = (video) => {
-    setSelectedVideo(video);
-  };
+  // const onVideoSelect = (video) => {
+  //   setSelectedVideo(video);
+  // };
   return (
     <div className="ui container">
       <SearchBar onFormSubmit={onTermSubmit} />
@@ -32,7 +32,10 @@ const App = () => {
             <VideoDetail video={selectedVideo} />
           </div>
           <div className="five wide column">
-            <VideoList onVideoSelect={onVideoSelect} videos={videos} />
+            <VideoList
+              onVideoSelect={(video) => setSelectedVideo(video)}
+              videos={videos}
+            />
           </div>
         </div>
       </div>
