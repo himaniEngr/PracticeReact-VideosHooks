12279b8b024d59538a60724568624f5986c9881c
Himani <himani9.engg@gmail.com>
2022-01-22 15:09:04 +0530

    refactoring search bar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,30 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
-class SearchBar extends React.Component {
-  state = { term: "" };
-  onInputChange = (event) => {
-    this.setState({ term: event.target.value });
-  };
-  onFormSubmit = (event) => {
+const SearchBar = ({ onFormSubmit }) => {
+  const [term, setTerm] = useState("");
+  // const onInputChange = (event) => {
+  //   setTerm(event.target.value);
+  // };
+  const onSubmit = (event) => {
     event.preventDefault();
-    this.props.onFormSubmit(this.state.term);
+    onFormSubmit(term);
   };
+  return (
+    <div className=" search-bar ui segment">
+      <form className="ui form" onSubmit={onSubmit}>
+        <div className="field">
+          <label>Video Search</label>
+          <input
+            type="text"
+            value={term}
+            onChange={(event) => setTerm(event.target.value)}
+          />
+        </div>
+      </form>
+    </div>
+  );
+};
 
-  render() {
-    return (
-      <div className=" search-bar ui segment">
-        <form className="ui form" onSubmit={this.onFormSubmit}>
-          <div className="field">
-            <label>Video Search</label>
-            <input
-              type="text"
-              value={this.state.term}
-              onChange={this.onInputChange}
-            />
-          </div>
-        </form>
-      </div>
-    );
-  }
-}
 export default SearchBar;
